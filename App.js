c30cd274ad180a47c396562131b816281711c9e3
Fransiskusandika <dikakoko@icloud.com>
2019-04-24 12:37:11 +0530

    wish

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from './app/components/Main';
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Main/>     
+    );
+  }
+}
+
+
